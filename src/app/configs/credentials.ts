8919191d4e2620f2fceeb5d493435aa99bcf2453
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-02-17 18:59:42 +0100

    credentials and database's rules
    copied credentials and database's rules from ng8balance

---
+++ src/app/configs/credentials.ts
@@ -0,0 +1,58 @@
+export const configs = {
+  firebase: {
+    apiKey: "AIzaSyCo8vHpRDMa_JsS5J6_vmLTbVNv8eMamgU",
+  authDomain: "fir-6062c.firebaseapp.com",
+  databaseURL: "https://fir-6062c.firebaseio.com",
+  projectId: "fir-6062c",
+  storageBucket: "fir-6062c.appspot.com",
+  messagingSenderId: "84418489236",
+  appId: "1:84418489236:web:7842bdbbd3fbac26"
+  },
+  google: {
+    api_key: 'AIzaSyClc8roQ21e1VFTzJTS0XkG7pVA_c3SGis'// 'AIzaSyClc8roQ21e1VFTzJTS0XkG7pVA_c3SGis'
+    
+  }
+};
+
+/* { fdirewbase's rules
+  "rules": {
+  
+        "categorie":{
+          "$uid":{
+            ".read":"$uid===auth.uid",
+              ".write":"$uid===auth.uid"
+          }
+        },
+          "fornitori":{
+          "$uid":{
+							".read":"$uid===auth.uid",
+              ".write":"$uid===auth.uid"
+          }
+        },
+          "acquisti":{
+          "$uid":{
+							".read":"$uid===auth.uid",
+              ".write":"$uid===auth.uid"
+          }
+        },
+          "userProfile":{
+          "$uid":{
+							".read":"$uid===auth.uid",
+              ".write":"$uid===auth.uid"
+          }
+        },
+          "widgets":{
+          "$uid":{
+							".read":"$uid===auth.uid",
+              ".write":"$uid===auth.uid"
+          }
+        },
+          "pagamenti":{
+          "$uid":{
+							".read":"$uid===auth.uid",
+              ".write":"$uid===auth.uid"
+          }
+        }
+
+}
+} */
