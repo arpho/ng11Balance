4e36bb1f40a00be40288479b3f8a4046d2717fa9
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-03-02 14:39:31 +0100

    shoppingkarts list on home page
    app-page-items-list put in folder.page.html
    #TODO: limit the list

--- src/app/folder/folder.module.ts
+++ src/app/folder/folder.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { FolderPageRoutingModule } from './folder-routing.module';
 
 import { FolderPage } from './folder.page';
+import { ItemModule } from '../modules/item/item.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FolderPageRoutingModule
+    FolderPageRoutingModule,
+    ItemModule
   ],
   declarations: [FolderPage]
 })
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ShoppingKartModel } from '../models/shoppingKartModel';
+import { ItemModelInterface } from '../modules/item/models/itemModelInterface';
+import { CreateShoppingKartPage } from '../pages/create-shopping-kart/create-shopping-kart.page';
+import { DetailShoppingKartPage } from '../pages/detail-shopping-kart/detail-shopping-kart.page';
+import { ShoppingKartsService } from '../services/shoppingKarts/shopping-karts.service';
 
 @Component({
   selector: 'app-folder',
@@ -8,11 +13,35 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FolderPage implements OnInit {
   public folder: string;
+  ItemsList: ItemModelInterface[];
+  filterLabel: string;
+  filterString: string;
+  secondSpinner: boolean;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  createModalPage= CreateShoppingKartPage
+  public editModalPage= DetailShoppingKartPage
+  filterFields: any;
+  compareDate = (a: Date, b: Date) => a > b ? -1 : a < b ? 1 : 0
+  filterFunction: (item: ItemModelInterface) => boolean;
+  sorterFunction: (a: ItemModelInterface, b: ItemModelInterface) => number =
+    (a: ShoppingKartModel, b: ShoppingKartModel) => {
+      // tslint:disable: semicolon
+      const dateA = a.purchaseDate.getFullDate()
+      const dateB = b.purchaseDate.getFullDate()
+      return this.compareDate(dateA, dateB)
+    }
+
+  constructor(private activatedRoute: ActivatedRoute,public service: ShoppingKartsService) { }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.secondSpinner = true
+    this.ItemsList = [];
+    this.service.items.subscribe(items=>{
+      this.ItemsList = items
+      this.secondSpinner = false
+    })
   }
 
 }
