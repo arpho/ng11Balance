e087c5a7e940ada5c01ca88c891d3910a9c8870a
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-03-18 20:03:15 +0100

    trying fixing problem on form
    no fixed yet

--- src/app/modules/dynamic-form/components/question-form-component/question-form-component.component.ts
+++ src/app/modules/dynamic-form/components/question-form-component/question-form-component.component.ts
@@ -7,18 +7,36 @@ import {
   ChangeDetectionStrategy,
   ɵConsole,
   OnChanges,
-  SimpleChanges
+  SimpleChanges,
+  forwardRef
 } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 import { QuestionBase } from '../../models/question-base';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question-form-component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  
 })
-export class QuestionFormComponent implements OnInit, OnChanges {
+export class QuestionFormComponent implements OnInit, OnChanges,ControlValueAccessor {
+
+  private onChange = () => { };
+  private onTouch = () => { };
+
+  writeValue(obj: any): void {
+    throw new Error('Method not implemented.');
+  }
+  registerOnChange(fn: any): void {
+   this.onChange = fn
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouch= fn
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
   public value: any;
   @Input() question: QuestionBase<any>;
   @Input() form: FormGroup;
