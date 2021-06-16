fc78c7e77f047e0022f46ce1a01c89bb5f93806c
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-06-16 17:22:30 +0200

    refactoring
    fixed routes for user's module

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,9 +1,53 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginPage } from './pages/login/login.page';
+import { AuthGuard } from './services/authguard.service';
+import { RoleGuardService } from './services/role-guards.service';
+
+const routes: Routes = [
+  {
+    path: "login",
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: "profile",
+    loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfilePageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "signup",
+    loadChildren: () => import("./pages/signup/signup.module").then(m => m.SignupPageModule)
+  },
+  {
+    path: "reset-password",
+    loadChildren: () => import(
+      "./pages/reset-password/reset-password.module").then(m => m.ResetPasswordPageModule),
+    canActivate: [AuthGuard]
+
+  },
+  {
+    path: "users",
+    loadChildren: () => import('./pages/users/users.module').then(m => m.UsersPageModule),
+    canActivate: [AuthGuard, RoleGuardService]
+  },
+  {
+    path: '',
+    redirectTo: 'users',
+    pathMatch: 'full'
+  },
+  {
+    path: "edit-user/:key",
+    loadChildren: () => import('./pages/edit-user/edit-user.module').then(m => m.EditUserPageModule),
+    canActivate: [AuthGuard]
+  }, 
+  {
+    path: "not-authorized/:message",
+    loadChildren: () => import('./pages/not-authorized/not-authorized.module').then(m => m.NotAuthorizedPageModule)
+  }
+];
 
 @NgModule({
   declarations: [LoginPage],
