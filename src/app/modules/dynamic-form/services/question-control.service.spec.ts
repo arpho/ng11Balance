6e22d3e5e921e186c229e7519923609d24fbdb6a
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-08-11 00:35:00 +0200

    test successfull
    provided FormBuilder to the testBed

--- src/app/modules/dynamic-form/services/question-control.service.spec.ts
+++ src/app/modules/dynamic-form/services/question-control.service.spec.ts
@@ -1,9 +1,10 @@
 import { TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
 
 import { QuestionControlService } from './question-control.service';
 
 describe('QuestionControlService', () => {
-  beforeEach(() => TestBed.configureTestingModule({ providers: [QuestionControlService] }));
+  beforeEach(() => TestBed.configureTestingModule({ providers: [QuestionControlService,FormBuilder] }));
 
   it('should be created', () => {
     const service: QuestionControlService = TestBed.get(QuestionControlService);
