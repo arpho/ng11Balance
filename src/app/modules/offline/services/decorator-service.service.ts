6ea565dc180c95712d4418ee06f0a9f941711b08
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-08-12 19:20:53 +0200

    decoratorService
    implemented service following stackOverflow

--- src/app/modules/offline/services/decorator-service.service.ts
+++ src/app/modules/offline/services/decorator-service.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { CategoriesService } from 'src/app/services/categories/categorie.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class DecoratorServiceService {
-
-  constructor() { }
+@Injectable()
+export class DecoratorService {
+     private static service: CategoriesService | undefined = undefined;
+     public constructor(service: CategoriesService) {
+         DecoratorService.service = service;
+     }
+     public static getService(): CategoriesService {
+         if(!DecoratorService.service) {
+             throw new Error('DecoratorService not initialized');
+         }
+         return DecoratorService.service;
+     }
 }
