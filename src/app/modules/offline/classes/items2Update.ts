ebc93c7000860e16c5d489fe7c3444f8abc00ace
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-08-16 20:26:12 +0200

    items2Update
    implemented model items2Update and operationKey

---
+++ src/app/modules/offline/classes/items2Update.ts
@@ -0,0 +1,7 @@
+import { ItemModelInterface } from "../../item/models/itemModelInterface";
+import { operationKey } from "./operationKey";
+
+export class items2Update {
+    item: ItemModelInterface;
+    operationKey: operationKey
+}
---
+++ src/app/modules/offline/classes/operationKey.ts
@@ -0,0 +1,6 @@
+export enum operationKey {
+    create = 1,
+    update,
+    delete
+
+}
