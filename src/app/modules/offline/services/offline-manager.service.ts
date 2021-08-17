c93856846ff48bd5a3c87cc20db6489410fbbf1c
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-08-17 15:50:20 +0200

    registerService
    implemnted registerService

--- src/app/modules/offline/services/offline-manager.service.ts
+++ src/app/modules/offline/services/offline-manager.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { OfflineItemServiceInterface } from '../models/offlineItemServiceInterface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OfflineManagerService {
-
+servicesList:Array<OfflineItemServiceInterface>=[]
   constructor() { }
+
+  registerService(service:OfflineItemServiceInterface){
+    this.servicesList.push(service)
+  }
 }
