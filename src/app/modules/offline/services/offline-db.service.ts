35c10c36de8a2c1e25f21a53cf8ba93ffc94f424
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-08-18 18:08:44 +0200

    implementing
    implemented offlineDbService
    #TODO complete fetchItem4Entity

--- src/app/modules/offline/services/offline-db.service.ts
+++ src/app/modules/offline/services/offline-db.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import * as localforage from 'localforage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OfflineDbService {
 
-  constructor() { }
+  constructor() { 
+    localforage.config({
+      storeName:'BalanceOfflineDb'
+    })
+  }
+
+
+  get(key: string) {
+    return localforage.getItem(key);
+  }
+
+  set(key: string, value: any) {
+    return localforage.setItem(key, value);
+  }
+
+  remove(key: string) {
+    return localforage.removeItem(key);
+  }
+
+  DELETE_ALL() {
+    return localforage.clear();
+  }
+
+  listKeys() {
+    return localforage.keys();
+  }
+
+  fetchAll4Entity(entity:string){
+    const out=[]
+    localforage.iterate((value,key,iterationNumber)=>{
+      if(value){}
+    })
+  }
+
 }
