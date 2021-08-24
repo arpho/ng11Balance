3203b73012671f06ea505c95d6b3d91017c38778
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-08-24 13:25:09 +0200

    redux and implementation
    installed redux implemnted part of the service

--- src/app/modules/offline/services/connection-status.service.ts
+++ src/app/modules/offline/services/connection-status.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-
+import { createStore, applyMiddleware } from 'redux' 
+import {reduxThunk} from 'redux-thunk'
 @Injectable({
   providedIn: 'root'
 })
@@ -9,5 +10,17 @@ export class ConnectionStatusService {
 
     //we create the listener
     window.addEventListener("isOnline", e => { console.log(navigator.onLine); });
+    // enable middleware
+    const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+    // create store
+    const store = createStoreWithMiddleware(reduxThunk);// reduxThunk just beacause I do not know what reducers are
+    //adding listner
+    function listenToWindowEvent(name, mapEventToAction) { 
+      return function (dispatch) { function handleEvent(e) { dispatch(mapEventToAction(e)); } window.addEventListener(name, handleEvent); } 
+    }
+
+    function navigatorOnLine(e) {
+       return { type: 'WEB_APP_ONLINE', payload: navigator.onLine };
+       }
   }
 }
