7908c967f5fa442a0c146b3623338637fc773f09
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-08-24 16:36:27 +0200

    reducer and store
    created reducer and store

--- src/app/modules/offline/services/connection-status.service.ts
+++ src/app/modules/offline/services/connection-status.service.ts
@@ -10,5 +10,24 @@ export class ConnectionStatusService {
   // Define an initial state value for the app
 const initialState = {
   value: navigator.onLine
+  
 
+}
+function reducer(state= initialState,action){
+  switch(action.type){
+    case "connection/online":
+      return{...state,value:navigator.onLine}
+      case "connection/offline":
+        return {...state,value:navigator.onLine}
+        default:
+          return state
+  }// Create a new Redux store with the `createStore` function,
+// and use the `reducer` for the update logic
+const store = createStore(reducer)
+
+
+}
+  
+
+}
 }
