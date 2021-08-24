b1a24fcd1a615164d58238914f3c1a0e4674ce58
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-08-24 16:23:25 +0200

    redux
    implementing a redux store
    https://redux.js.org/tutorials/fundamentals/part-1-overview

--- src/app/modules/offline/services/connection-status.service.ts
+++ src/app/modules/offline/services/connection-status.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { createStore, applyMiddleware } from 'redux' 
-import {reduxThunk} from 'redux-thunk'
 @Injectable({
   providedIn: 'root'
 })
@@ -8,19 +7,8 @@ export class ConnectionStatusService {
 
   constructor() { 
 
-    //we create the listener
-    window.addEventListener("isOnline", e => { console.log(navigator.onLine); });
-    // enable middleware
-    const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
-    // create store
-    const store = createStoreWithMiddleware(reduxThunk);// reduxThunk just beacause I do not know what reducers are
-    //adding listner
-    function listenToWindowEvent(name, mapEventToAction) { 
-      return function (dispatch) { function handleEvent(e) { dispatch(mapEventToAction(e)); } window.addEventListener(name, handleEvent); } 
-    }
+  // Define an initial state value for the app
+const initialState = {
+  value: navigator.onLine
 
-    function navigatorOnLine(e) {
-       return { type: 'WEB_APP_ONLINE', payload: navigator.onLine };
-       }
-  }
 }
