7a39fae981f1f0b360c8de7fab1df8b70b6c0bb9
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-08-25 16:49:41 +0200

    refactoring
    corrected logic of storeSignatureOnLocalDb

--- src/app/modules/offline/business/storeSignatureOnLocalDb.spec.ts
+++ src/app/modules/offline/business/storeSignatureOnLocalDb.spec.ts
@@ -3,6 +3,7 @@ import {LocalForageMocker} from '../services/mockers/offlineDbServiceMocker'
 import { waitForAsync } from '@angular/core/testing'
 
 const db:LocalForageMocker = new LocalForageMocker()
+const signature = 'Qfe9bcGcUxTIKmL9ccn76z12gzE2_Linux x86_64_Chrome'
 describe('storing signature',()=>{
     beforeEach(waitForAsync(()=>{
     }))
@@ -14,15 +15,23 @@ describe('storing signature',()=>{
         const sign0 = await db.get('signature_0')
         
         
-        expect (sign0.signature).toEqual('Qfe9bcGcUxTIKmL9ccn76z12gzE2_Linux x86_64_Chrome')
+        expect (sign0.signature).toEqual(signature)
     })
     it('previous signatures',async ()=>{
+        await db.set("signature_0",{signature:'Qfe9bcGcUxTIKmL9ccn76z12gzE2_Linux x86_64_Chrome',entityLabel:'signatures',lastUsed:false})// setto una firma precedente con uguale firma e lastused falso
+        await db.set("signature_1",{signature:'qwertygggggggggggggggggggggc_Linux x86_64_Chrome',entityLabel:'signatures',lastUsed:true})// setto una firma precedente con  firma diversa ma lastused vero
         const store= new StoreSignature(db,'Qfe9bcGcUxTIKmL9ccn76z12gzE2_Linux x86_64_Chrome')
         await store.execute()
+        console.log('db#',db)
+        const signaturesList = await  db.fetchAllRawItems4Entity('signatures')
+        console.log('#signaturesList',signaturesList)
+        console.log('signs #',signaturesList.map(sign=>sign['item']['signature']))
+        console.log('# already there',signaturesList.map(sign=>sign['item']['signature']).includes(signature))
         const signs = await db.fetchAllRawItems4Entity('signatures')
-        expect(signs.length).toEqual(2)
-        const sign1 = await db.get('signature_1')
-        expect(sign1.signature).toEqual('Qfe9bcGcUxTIKmL9ccn76z12gzE2_Linux x86_64_Chrome')
+        expect(signs.length).toEqual(1)
+        const sign0 = await db.get('signature_0')
+        expect(sign0.signature).toEqual(signature)
+        expect(sign0.lastUsed).toBeTrue()
 
 
     })
--- src/app/modules/offline/business/storeSignatureOnLocalDb.ts
+++ src/app/modules/offline/business/storeSignatureOnLocalDb.ts
@@ -10,8 +10,17 @@ export class StoreSignature{
     }
 
     async execute(){
-       const signaturesList = await  this.db.fetchAllRawItems4Entity('signatures')
-           this.db.set(`signature_${signaturesList.length}`,{signature:this.signature,entityLabel:'signatures'})//@TODO if the signature is already present I set this as last used
+        const signaturesList = await  this.db.fetchAllRawItems4Entity('signatures')
+       
+       if(signaturesList.map(sign=>sign['item']['signature']).includes(this.signature)){
+           console.log('firma gia prersente #')
+           const item = signaturesList.filter(item=>item.item['signature']==this.signature)[0]
+           console.log('item to set #',item)
+           this.db.set(item.key,{ key:item.key,signature:item.item['signature'],lastUsed:true})
+           console.log('db #',this.db)
+       }
+     
+           //this.db.set(`signature_${signaturesList.length}`,{signature:this.signature,entityLabel:'signatures'})//@TODO if the signature is already present I set this as last used
        
       
     }
