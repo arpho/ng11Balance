a9a2db67e66fab889cecbf939bfa2edc605adc27
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-08-25 16:58:11 +0200

    test succesfull

--- src/app/modules/offline/business/storeSignatureOnLocalDb.spec.ts
+++ src/app/modules/offline/business/storeSignatureOnLocalDb.spec.ts
@@ -2,10 +2,10 @@ import {StoreSignature, } from './storeSignatureOnLocalDb'
 import {LocalForageMocker} from '../services/mockers/offlineDbServiceMocker'
 import { waitForAsync } from '@angular/core/testing'
 
-const db:LocalForageMocker = new LocalForageMocker()
+var db:LocalForageMocker = new LocalForageMocker()
 const signature = 'Qfe9bcGcUxTIKmL9ccn76z12gzE2_Linux x86_64_Chrome'
 describe('storing signature',()=>{
-    beforeEach(waitForAsync(()=>{
+    beforeEach(waitForAsync(()=>{db= new LocalForageMocker()
     }))
     it('no previous signature',async ()=>{
         const store= new StoreSignature(db,'Qfe9bcGcUxTIKmL9ccn76z12gzE2_Linux x86_64_Chrome')
@@ -22,12 +22,16 @@ describe('storing signature',()=>{
         await db.set("signature_1",{signature:'qwertygggggggggggggggggggggc_Linux x86_64_Chrome',entityLabel:'signatures',lastUsed:true})// setto una firma precedente con  firma diversa ma lastused vero
         const store= new StoreSignature(db,'Qfe9bcGcUxTIKmL9ccn76z12gzE2_Linux x86_64_Chrome')
         await store.execute()
+        console.log('db#',db)
         const signaturesList = await  db.fetchAllRawItems4Entity('signatures')
+        console.log('#signaturesList',signaturesList)
+        console.log('signs #',signaturesList.map(sign=>sign['item']['signature']))
+        console.log('# already there',signaturesList.map(sign=>sign['item']['signature']).includes(signature))
         const signs = await db.fetchAllRawItems4Entity('signatures')
         expect(signs.length).toEqual(1)
-        const sign0 = await db.get('signature_0')
-        expect(sign0.signature).toEqual(signature)
-        expect(sign0.lastUsed).toBeTrue()
+        const sign1 = await db.get('signature_0')
+        expect(sign1.signature).toEqual(signature)
+        expect(sign1.lastUsed).toBeTrue
 
 
     })
