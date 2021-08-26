305ef1f4f85e10fd6ff2c7514fe4e99f69b13937
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-08-26 18:00:38 +0200

    updateEntityOffilne
    implemented all method

---
+++ src/app/modules/offline/business/updateEntityOffline.ts
@@ -0,0 +1,23 @@
+import { ItemModelInterface } from "../../item/models/itemModelInterface";
+import { OfflineItemModelInterface } from "../models/offlineItemModelInterface";
+import { OfflineDbService } from "../services/offline-db.service";
+
+export class UpdateEntityOffline {
+    entity: OfflineItemModelInterface
+    db: OfflineDbService
+    constructor(entity: OfflineItemModelInterface, db: OfflineDbService) {
+        this.db = db
+        this.entity = entity
+    }
+
+    async execute(isOnline:boolean){
+        await this.db.set(this.entity.key,{...this.entity.serialize4OfflineDb()}) 
+        if(!isOnline){// se online non serve registrare la modifica
+
+        }
+        else{
+            // registro la modifica che sarà riportata onLine appena possibile
+            this.db.set(new Date().getTime+'',{entityLabel:'update','entity':this.entity.serialize4OfflineDb()})
+        }
+    }
+}
