361fc30f1c3ac960f298502f7ccdff1ab6b07f3e
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-08-26 18:32:05 +0200

    bug fixing: tests ok
    corrected rule

--- src/app/modules/offline/business/updateEntityOffline.spec.ts
+++ src/app/modules/offline/business/updateEntityOffline.spec.ts
@@ -19,15 +19,14 @@ describe("we should update an entity offline and when needed  add an update item
         db.set(categoryTest.key,categoryTest.serialize4OfflineDb())
         categoryTest.title='test'
         await new UpdateEntityOffline(categoryTest,db).execute(true)
-        it("entity correctly updated",async ()=>{
-            const rawCategory = await db.get(categoryTest.key)
-            console.log('raw updated cat',rawCategory)
-            const updatedCategory = new CategoryModel().initialize(rawCategory)
-            expect(rawCategory.entityLabel).toEqual(categoryTest.entityLabel)
-            expect(updatedCategory.title).toEqual('test')
-            const updated = await db.fetchAllRawItems4Entity('update')
-            expect(updated.length).toEqual(0)
-
-        })
+        const rawCategory = await db.get(categoryTest.key)
+        console.log('raw updated cat',rawCategory)
+        const updatedCategory = new CategoryModel().initialize(rawCategory)
+        expect(rawCategory.entityLabel).toEqual(categoryTest.entityLabel)
+        expect(updatedCategory.title).toEqual('test')
+        const updated = await db.fetchAllRawItems4Entity('update')
+        expect(updated.length).toEqual(0)
+        
     })
+  
 })
