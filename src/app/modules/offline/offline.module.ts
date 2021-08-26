2b79b06eaa934653e365de732b8aeb6ce594d7aa
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-08-26 19:46:18 +0200

    decoratorFactory
    removed unused class

--- src/app/modules/offline/offline.module.ts
+++ src/app/modules/offline/offline.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { DecoratorService } from './services/decorator-service.service';
 import { OfflineStatusComponent } from './components/offline-status/offline-status.component';
 import { IonicModule } from '@ionic/angular';
-import { DecoratorFactoryService } from './services/decorator-factory.service';
 import { ConnectionStatusService } from './services/connection-status.service';
 
 
@@ -11,7 +10,7 @@ import { ConnectionStatusService } from './services/connection-status.service';
 @NgModule({
   declarations: [OfflineStatusComponent],
   exports: [OfflineStatusComponent],
-  providers: [DecoratorService, DecoratorFactoryService, ConnectionStatusService],
+  providers: [DecoratorService, ConnectionStatusService],
   imports: [
     CommonModule,
     IonicModule.forRoot(),
