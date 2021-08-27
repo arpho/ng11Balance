9ed51266c8a05472074b7a9770e3cd6419067cf2
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-08-27 07:14:13 +0200

    createEntityOffline
    created class to create the item on local db

---
+++ src/app/modules/offline/business/createEntityOffline.ts
@@ -0,0 +1,29 @@
+import { ItemModelInterface } from "../../item/models/itemModelInterface";
+import { Items2Update } from "../models/items2Update";
+import { OfflineItemModelInterface } from "../models/offlineItemModelInterface";
+import { OperationKey } from "../models/operationKey";
+import { ChangesService } from "../services/changes.service";
+import { OfflineDbService } from "../services/offline-db.service";
+import { OfflineManagerService } from "../services/offline-manager.service";
+
+export class UpdateEntityOffline {
+    entity: OfflineItemModelInterface
+    db: OfflineDbService
+    constructor(entity: OfflineItemModelInterface, db: OfflineDbService) {
+        this.db = db
+        this.entity = entity
+    }
+
+    async execute(isOnline: boolean) {
+        await this.db.set(this.entity.key, { ...this.entity.serialize4OfflineDb() })
+        if (isOnline) {// se online non serve registrare la modifica sul db locale
+            const Item2Update = new Items2Update(this.entity, OperationKey.update)
+            ChangesService.createItem(Item2Update)
+        }
+        else {
+            // registro la modifica che sarà riportata onLine appena possibile
+            await this.db.set(new Date().getTime() + '', { entityLabel: 'update', operation: OperationKey.create, 'entity': this.entity.serialize4OfflineDb() })
+        }
+        OfflineManagerService.publishEntity(this.entity.entityLabel)
+    }
+}
