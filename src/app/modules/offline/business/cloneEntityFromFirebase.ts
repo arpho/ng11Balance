ff53eebeb2d48e7cc8c7d1dfcb0861c1b6ff7d9a
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-08-30 04:37:35 +0200

    refactoring for debug

--- src/app/modules/offline/business/cloneEntityFromFirebase.ts
+++ src/app/modules/offline/business/cloneEntityFromFirebase.ts
@@ -11,7 +11,7 @@ export class CloneEntity {
     this.service = service
   }
   async execute() {
-    console.time('fetching')
+    console.time(`fetching from ${this.service.entityLabel}`)
     await this.service.fetchItemsFromCloud(async (items) => {
       items.forEach(async (item: RawItem) => {
         item.item['entityLabel'] = this.service.entityLabel
@@ -21,7 +21,7 @@ export class CloneEntity {
       await this.db.set(`${this.service.entityLabel}_status_db`, offLineDbStatus.up2Date)
       console.log('synced', this.service.entityLabel)
       this.service.offlineDbStatus = offLineDbStatus.up2Date
-      console.timeEnd('fetching')
+      console.timeEnd(`fetching from ${this.service.entityLabel}`)
     })
   }
 
--- src/app/modules/offline/services/offline-manager.service.ts
+++ src/app/modules/offline/services/offline-manager.service.ts
@@ -19,6 +19,7 @@ export class OfflineManagerService {
 
 
   constructor(public localDb: OfflineDbService, public users: UsersService) {
+   
 
     this.makeSignature(async sign => {
 
