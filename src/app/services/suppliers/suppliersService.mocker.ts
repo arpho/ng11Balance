648a83b009cb8af3a02c2b31daca7d8a087e4701
arpho <damicogiuseppe77@gmail.com>
2021-09-12 11:27:04 +0200

    mocker for suppliersService
    Signed-off-by: arpho <damicogiuseppe77@gmail.com>

--- src/app/services/suppliers/suppliersService.mocker.ts
+++ src/app/services/suppliers/suppliersService.mocker.ts
@@ -1,3 +1,5 @@
+import { SupplierModel } from "src/app/models/supplierModel";
+import { OfflineItemModelInterface } from "src/app/modules/offline/models/offlineItemModelInterface";
 import { ChangesService } from "src/app/modules/offline/services/changes.service";
 import { OfflineDbService } from "src/app/modules/offline/services/offline-db.service";
 import { OfflineManagerService } from "src/app/modules/offline/services/offline-manager.service";
@@ -6,5 +8,22 @@ import { SuppliersService } from "./suppliers.service";
 export class SuppliersServiceMocker extends SuppliersService{
     constructor(localDb:OfflineDbService,manager:OfflineManagerService,changes:ChangesService){
         super(localDb,manager,changes)
+    }
+
+    db = {}
+
+    async createItem(item: OfflineItemModelInterface) {
+        this.db[item.key] = item
+        return new SupplierModel().initialize(item)
+
+    }
+
+    async updateItem(item: OfflineItemModelInterface) {
+        this.db[item.key] = item
+
+    }
+
+    async deleteItem(key: string) {
+        delete this.db[key]
     }
 }
