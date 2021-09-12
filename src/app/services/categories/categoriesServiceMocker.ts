cbaa54cd6c88188e7b0a585cebcd70437a3b150f
arpho <damicogiuseppe77@gmail.com>
2021-09-12 11:22:57 +0200

    categoriesMocker implemented  mocker for categories
    Signed-off-by: arpho <damicogiuseppe77@gmail.com>

---
+++ src/app/services/categories/categoriesServiceMocker.ts
@@ -0,0 +1,28 @@
+import { OfflineItemModelInterface } from "src/app/modules/offline/models/offlineItemModelInterface";
+import { ChangesService } from "src/app/modules/offline/services/changes.service";
+import { OfflineDbService } from "src/app/modules/offline/services/offline-db.service";
+import { OfflineManagerService } from "src/app/modules/offline/services/offline-manager.service";
+import { UsersService } from "src/app/modules/user/services/users.service";
+import { CategoriesService } from "./categorie.service";
+
+export class CategoriesServiceMocker extends CategoriesService {
+    constructor(manager: OfflineManagerService, localDb: OfflineDbService, changes: ChangesService, users: UsersService) {
+        super(manager, users, localDb, changes)
+    }
+
+    db = {}
+
+    async createItem(item: OfflineItemModelInterface) {
+        this.db[item.key] = item
+
+    }
+
+    async updateItem(item: OfflineItemModelInterface) {
+        this.db[item.key] = item
+
+    }
+
+    async deleteItem(key: string) {
+        delete this.db[key]
+    }
+}
