9aded54d0b0c298c9220f2579cab8793935ff3ab
arpho <damicogiuseppe77@gmail.com>
2021-09-15 16:36:24 +0200

    refactoring created class o handle the process of pulling changes from firebase
    Signed-off-by: arpho <damicogiuseppe77@gmail.com>

---
+++ src/app/modules/offline/business/puller.ts
@@ -0,0 +1,55 @@
+import { Items2Update } from "../models/items2Update";
+import { OfflineItemServiceInterface } from "../models/offlineItemServiceInterface";
+import { OperationKey } from "../models/operationKey";
+import { RawItem } from "../models/rawItem";
+import { ChangesService } from "../services/changes.service";
+import { OfflineDbService } from "../services/offline-db.service";
+
+export class Puller {
+    changes: Items2Update[]
+    Changes: ChangesService
+    signature: string
+    services: OfflineItemServiceInterface[]
+    localDb: OfflineDbService
+    constructor(localDb: OfflineDbService, signature: string, services: OfflineItemServiceInterface[], Changes: ChangesService) {
+        this.localDb = localDb
+        this.signature = signature
+        this.services = services
+        this.Changes = Changes
+    }
+
+    async fetchChangesFromFirebase() {
+
+        const entitiesRestore = (items: RawItem[]) =>/**
+        initializes the entities inside the changes
+        */ {
+            this.changes = []
+            items.forEach(item => {
+                const Service = this.services.filter(service => service.entityLabel == item.item['entityLabel2Update'])[0]
+                const entity = Service.getDummyItem().initialize(item.item)
+                const change = new Items2Update(item['owner'], entity, item['operationKey']).setItem(entity)
+                this.changes.push(change)
+            })
+
+        }
+        this.Changes.fetchItemsFromCloud(entitiesRestore)
+        return this
+    }
+
+    async storeChanges() {
+        this.changes.forEach(async change => {
+            if (change.operationKey == OperationKey.create) {
+                await this.localDb.set(change.item.key, change.item.serialize4OfflineDb())
+            }
+            if (change.operationKey == OperationKey.update) {
+                await this.localDb.set(change.item.key, change.item.serialize4OfflineDb())
+            }
+            if (change.operationKey == OperationKey.delete) {
+                await this.localDb.remove(change.item.key)
+            }
+            change.sign(this.signature)
+        })
+        return this
+    }
+
+}
