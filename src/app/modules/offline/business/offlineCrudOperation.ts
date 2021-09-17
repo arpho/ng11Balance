a9d4edcf6222ef4b41da8b9b68ff467d0bac2496
arpho <damicogiuseppe77@gmail.com>
2021-09-17 12:30:03 +0200

    refactoring crud operations offline were messy implementing a new series of classes, as fare implemented the base class,refactoring the interface offlineCrudOperations
    Signed-off-by: arpho <damicogiuseppe77@gmail.com>

--- src/app/modules/offline/business/offlineCrudOperation.ts
+++ src/app/modules/offline/business/offlineCrudOperation.ts
@@ -1,12 +1,40 @@
+import { threadId } from "worker_threads";
 import { Items2Update } from "../models/items2Update";
 import { OfflineItemModelInterface } from "../models/offlineItemModelInterface";
+import { OperationKey } from "../models/operationKey";
 import { ChangesService } from "../services/changes.service";
 import { OfflineDbService } from "../services/offline-db.service";
 
-export interface offlineCrudOperation{
+export class offlineCrudOperation{
     changes: ChangesService
     signature: string
     item: OfflineItemModelInterface
-    item2Update: Items2Update
     localDb: OfflineDbService
+    userOfflineEnabled= false
+    constructor(changes:ChangesService,localDb:OfflineDbService,item:OfflineItemModelInterface,userOfflineEnabled:boolean,signature:string){
+        this.changes= changes
+        this.localDb= localDb
+        this.item = item
+        this.signature = signature
+        this.userOfflineEnabled= userOfflineEnabled
+        
+    }
+
+    async applyOnLocalDb(){
+        if(this.userOfflineEnabled){
+       await this.localDb.set(this.item.key,this.item.serialize4OfflineDb())}
+        return this
+    }
+
+   async  createsChange(){
+       const change = new Items2Update(this.signature,this.item,OperationKey.create)
+       await this.changes.createItem(change)
+       return this
+   }
+
+   async runOperations(){
+     (await this.applyOnLocalDb()).createsChange()
+       return this.item
+   }
+
 }
