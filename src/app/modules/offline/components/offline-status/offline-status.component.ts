7d9c46be9f380b9936b17aa42789b3d29ef57cbb
arpho <damicogiuseppe77@gmail.com>
2021-09-25 16:42:27 +0200

    refactoring moved offline component  to the end of  toolbar
    Signed-off-by: arpho <damicogiuseppe77@gmail.com>

--- src/app/modules/offline/components/offline-status/offline-status.component.ts
+++ src/app/modules/offline/components/offline-status/offline-status.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { OfflineManagerService } from '../../services/offline-manager.service';
 
 @Component({
@@ -10,14 +11,40 @@ import { OfflineManagerService } from '../../services/offline-manager.service';
 export class OfflineStatusComponent implements OnInit {
   status = OfflineManagerService.offlineDbStatus
 
-  constructor() {
+  constructor(public alertController:AlertController) {
 
   }
 
   ngOnInit() { }
 
 
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Confirm!',
+      message: 'Sei sicuro di voler syncronizzare il   <strong>Db locale?</strong>!!!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'sincronizza',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+
   refresh() {
-    console.log('refresh db')
+    this.presentAlertConfirm()
   }
 }
