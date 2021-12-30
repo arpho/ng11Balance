3fab5e3019f1e5d4d6c9ab22af64ac064bc306e8
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-12-30 16:12:50 +0100

    refactoring
    refactoring

--- src/app/modules/offline/business/LastLoggedUidFetcher.ts
+++ src/app/modules/offline/business/LastLoggedUidFetcher.ts
@@ -1,14 +1,30 @@
+import { RawItem } from "../models/rawItem";
 import { OfflineDbService } from "../services/offline-db.service";
 
 export class LastLoggedUidFetcher {
+    signatures:RawItem[]
     db: OfflineDbService
+    lastLoggedSignature:RawItem
     constructor(db: OfflineDbService) {
         this.db = db
     }
 
-    async execute() {
-        const signatures = await this.db.fetchAllRawItems4Entity('signatures')
-        const lastLoggedSignature = signatures.filter(item => item.item['lastLogged'] == true)[0]
-        return lastLoggedSignature['signature'].split('_')[0]
+    async fetchSignatures(){
+        this.signatures = await this.db.fetchAllRawItems4Entity('signatures')
+        return this
+    }
+
+    getLastLogged(){
+        const filter = (item:RawItem) => item.item['lastLogged']== true
+        this.lastLoggedSignature = this.signatures.filter(filter)[0]
+        return this
+    }
+
+    extractUid(){
+        return this.lastLoggedSignature['signature'].split('_')[0]
+    }
+
+     async execute() {
+        return await (await this.fetchSignatures()).getLastLogged().extractUid()
     }
 }
