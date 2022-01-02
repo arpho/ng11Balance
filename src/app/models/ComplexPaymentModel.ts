a4778b777dda870f4514a787331752edac9d066e
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2022-01-02 18:39:49 +0100

    refactoring and implementing
    implemented new class
    #TODO to be tested

--- src/app/models/ComplexPaymentModel.ts
+++ src/app/models/ComplexPaymentModel.ts
@@ -1,10 +1,11 @@
 import { DateModel } from "../modules/user/models/birthDateModel";
 import { PaymentsModel } from "./paymentModel";
 
-export class ComplexPayment extends PaymentsModel {
+export class ComplexPaymentModel extends PaymentsModel {
     amount = 0
     paymentDate: DateModel
-    constructor(data: {  amount: 0, paymentDate: string,}) {
+    paymentKey: string
+    constructor(data?: {  amount: 0, paymentDate: string,}) {
         super()
         this.initialize(data)
 
@@ -15,7 +16,18 @@ export class ComplexPayment extends PaymentsModel {
         this.paymentDate = new DateModel(data.paymentDate)
         return this
     }
+    setPaymentKey(key:string){
+        this.paymentKey= key
+    }
 
+    setAmount(amount){
+        this.amount = amount
+    }
+
+    setDate(date:DateModel){
+        this.paymentDate= date
+
+    }
     serialize(){
         return {...this.serialize(),amount:this.amount,payment:this.paymentDate.formatDate()}
     }
---
+++ src/app/models/extendedShoppingKart.ts
@@ -0,0 +1,39 @@
+import { ComplexPaymentModel } from "./ComplexPaymentModel";
+import { ShoppingKartModel } from "./shoppingKartModel";
+
+export class ExtendedShoppingKartModel extends ShoppingKartModel{
+    payments:ComplexPaymentModel[]
+    constructor(data?){
+        super(data)
+        this.initialize(data)
+
+    }
+
+    initialize(cart: any): this {
+        super.initialize(cart)
+        if(cart.payments){// kart esteso, ha i pagamenti complessi
+            this.payments =[]
+            cart.array.forEach(element => {
+                const payment = new ComplexPaymentModel(element)
+                this.payments.push(payment)
+                
+            });
+        }
+        else{// retro compatibilità
+            const payment= new ComplexPaymentModel()
+            payment.setKey(this.pagamento.key)
+            payment.setAmount(this.totale)
+            payment.setDate(this.purchaseDate)
+            this.payments=[payment]
+        }
+        return this
+    }
+    payedAmount(){
+        const mapper =(item:ComplexPaymentModel)=> item.amount 
+        const reducer = (pv,cv)=>pv+cv
+        return this.payments.map(mapper).reduce(reducer,0)
+    }
+    isFullyPayed(){
+        return this.totale<= this.payedAmount()
+    }
+}
