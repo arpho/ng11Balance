d699562f9bdb912f58af3edd1a416cb855e10fad
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2022-12-31 19:19:41 +0100

    refactored listProperties
    it requires now the list of items to be shown

--- src/app/modules/dynamic-form/models/listProperties.ts
+++ src/app/modules/dynamic-form/models/listProperties.ts
@@ -1,6 +1,8 @@
+import { ItemsList } from "./itemsList";
 import { QuestionProperties } from "./questionproperties";
 
 export interface ListQuestionProperties extends QuestionProperties<unknown>{
     createPopup:unknown
     editPopup:unknown
+    itemsList:ItemsList[]
 }
