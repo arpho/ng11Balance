73e7a8365a86ee6eafdd0117366ae28db7f71cc3
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2023-01-01 16:30:42 +0100

    edit-payment-page ready to show a form

--- src/app/pages/complexPayments/edit-payment/edit-payment.page.ts
+++ src/app/pages/complexPayments/edit-payment/edit-payment.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
+import { ModalController, NavParams } from '@ionic/angular';
+import { TextboxQuestion } from 'src/app/modules/dynamic-form/models/question-textbox';
 
 @Component({
   selector: 'app-edit-payment',
@@ -7,13 +8,24 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./edit-payment.page.scss'],
 })
 export class EditPaymentPage implements OnInit {
+  questions=[new TextboxQuestion({key:"title",label:"test"})]
 
-  constructor(private modalCtrl:ModalController) { }
+  constructor(private modalCtrl:ModalController,
+    private navParams:NavParams) { }
 
   ngOnInit() {
+    const data  = this.navParams.get("data")
+    console.log("data",data)
   }
   dismiss(value?:unknown){
     this.modalCtrl.dismiss(value)
+  }
+  filter(ev){
+    console.log("typing",ev)
+  }
+
+  submit(ev){
+    console.log("submit",ev)
   }
 
 }
