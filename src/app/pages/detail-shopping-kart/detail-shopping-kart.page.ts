874bd50d690c7c1497f28195a016fbb9f61b11e1
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2023-01-01 17:03:53 +0100

    typo fixed
    used the correct field name

--- src/app/pages/detail-shopping-kart/detail-shopping-kart.page.ts
+++ src/app/pages/detail-shopping-kart/detail-shopping-kart.page.ts
@@ -118,7 +118,7 @@ export class DetailShoppingKartPage implements OnInit {
     createPage:CreatePaymentPage,
     editPage:EditPaymentPage,
     key:"payments",
-    itemsList:this.kart.payments.map(item=>{return {"title":item.title,"field2":this.roundPipe.transform( item.amount),"field3":item.paymentDate.formatDate(),itemkey:item.paymentKey}}) //converts the data in Itemslist
+    itemsList:this.kart.payments.map(item=>{return {"title":item.title,"field2":this.roundPipe.transform( item.amount),"field3":item.paymentDate.formatDate(),itemKey:item.key}}) //converts the data in Itemslist
     })
     ];
 
