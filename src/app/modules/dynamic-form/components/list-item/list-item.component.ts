85196ac8a016962515aa704190007b66ccce9ebd
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2023-01-03 17:11:04 +0100

    generated list item component

---
+++ src/app/modules/dynamic-form/components/list-item/list-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
+
+@Component({
+  selector: 'app-list-item',
+  templateUrl: './list-item.component.html',
+  styleUrls: ['./list-item.component.scss'],
+})
+export class ListItemComponent implements OnInit {
+
+  constructor(public viewContainerRef: ViewContainerRef) { }
+
+  ngOnInit() {}
+
+}
