f790d7fe1b44e19171f287771aeca08517c581fb
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2023-01-03 20:06:08 +0100

    working to inject component at runtime

--- src/app/modules/dynamic-form/components/items-host/items-host.component.ts
+++ src/app/modules/dynamic-form/components/items-host/items-host.component.ts
@@ -1,4 +1,4 @@
-import { Component, ComponentFactoryResolver, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, ComponentFactoryResolver, Input, OnInit, ViewChild, ViewChildren, ViewContainerRef } from '@angular/core';
 import { ItemHostDirective } from '../../directives/item-host.directive';
 import { ItemsListInterface } from '../../models/itemlistInterface';
 
@@ -10,17 +10,27 @@ import { ItemsListInterface } from '../../models/itemlistInterface';
 export class ItemsHostComponent implements OnInit {
 @Input() item:unknown
 @Input() itemComponent
-@ViewChild(ItemHostDirective, {static: true}) itemHost!: ItemHostDirective;
+@ViewChildren('itemHost', { read: ViewContainerRef }) itemHost!: ItemHostDirective;
   constructor(private resolver:ComponentFactoryResolver) { }
+   load(){
 
-  ngOnInit() {
-
-    const _viewContainerRef = this.itemHost.viewContainerRef;
+    const _viewContainerRef = this.itemHost['_results'][0]
       //removes all views in that container
       _viewContainerRef.clear();
+      console.log("itemComponent",this.itemComponent)
       const itemRef = _viewContainerRef.createComponent<ItemsListInterface>(this.itemComponent);
       // pass data to the component
     itemRef['item']= this.item
   }
 
+  ngAfterViewInit() {
+    console.log('on after view init', this.itemHost);
+    const ref = this.itemHost['_results'][0]
+    console.log("ref",ref)
+this.load()
+  }
+
+  ngOnInit() {
+    console.log('on after  init', this.itemHost);
+  }
 }
