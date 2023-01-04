fc034eeba8f5792ab87e08fad49223c579ad4888
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2023-01-04 20:43:47 +0100

    imported DynamicFormModule in complexpayment/createPaymentPage

--- src/app/pages/complexPayments/create-payment/create-payment.module.ts
+++ src/app/pages/complexPayments/create-payment/create-payment.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { CreatePaymentPageRoutingModule } from './create-payment-routing.module';
 
 import { CreatePaymentPage } from './create-payment.page';
+import { DynamicFormModule } from 'src/app/modules/dynamic-form/dynamic-form.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    DynamicFormModule,
     CreatePaymentPageRoutingModule
   ],
   declarations: [CreatePaymentPage]
