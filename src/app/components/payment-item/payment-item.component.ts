dc43b16eaece95c7ad46cffa8a67584be6713248
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2023-01-04 20:31:34 +0100

    debugging
    trying to show itemPayment

--- src/app/components/payment-item/payment-item.component.ts
+++ src/app/components/payment-item/payment-item.component.ts
@@ -1,14 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { ComplexPaymentModel } from 'src/app/models/ComplexPaymentModel';
 
 @Component({
   selector: 'app-payment-item',
   templateUrl: './payment-item.component.html',
   styleUrls: ['./payment-item.component.scss'],
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
-export class PaymentItemComponent implements OnInit {
-@Input() item:unknown
-  constructor() { }
+export class PaymentItemComponent implements OnInit, OnChanges{
+@Input() item:ComplexPaymentModel
+  constructor() { 
+    console.log("constructor of paymwnt item")
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("changes",changes)
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log("payment",this.item)
+  }
 
 }
--- src/app/modules/dynamic-form/components/items-host/items-host.component.ts
+++ src/app/modules/dynamic-form/components/items-host/items-host.component.ts
@@ -12,25 +12,31 @@ export class ItemsHostComponent implements OnInit {
 @Input() itemComponent
 @ViewChildren('itemHost', { read: ViewContainerRef }) itemHost!: ItemHostDirective;
   constructor() { }
-   load(){
-
+   async load(){
     const _viewContainerRef = this.itemHost['_results'][0]
       //removes all views in that container
       _viewContainerRef.clear();
       console.log("itemComponent",this.itemComponent)
       const itemRef = _viewContainerRef.createComponent<ItemsListInterface>(this.itemComponent);
+      console.log("item created")
       // pass data to the component
-    itemRef['item']= this.item
+    itemRef.instance.item= this.item
+    console.log("loaded",itemRef)
   }
 
+   delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
   ngAfterViewInit() {
-    console.log('on after view init', this.itemHost);
+    console.log('on after view init', this.itemHost,this.item);
     const ref = this.itemHost['_results'][0]
-    console.log("ref",ref)
+    console.log("ref",ref,this.item)
+    
 this.load()
   }
 
   ngOnInit() {
-    console.log('on after  init', this.itemHost);
+    console.log('on after  init', this.itemHost,this.item);
   }
 }
