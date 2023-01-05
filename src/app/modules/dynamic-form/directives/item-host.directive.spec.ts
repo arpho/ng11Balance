a79b1fd60bd8f8c6e836f3349a119a997160ef7a
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2023-01-05 01:25:50 +0100

    tested itemHost directive

--- src/app/modules/dynamic-form/directives/item-host.directive.spec.ts
+++ src/app/modules/dynamic-form/directives/item-host.directive.spec.ts
@@ -1,8 +1,11 @@
+import { ViewContainerRef } from '@angular/core';
 import { ItemHostDirective } from './item-host.directive';
+import { TestViewContainerRef } from './TestViewContainerRef';
 
 describe('ItemHostDirective', () => {
   it('should create an instance', () => {
-    const directive = new ItemHostDirective();
+    const ref = new TestViewContainerRef()
+    const directive = new ItemHostDirective(ref);
     expect(directive).toBeTruthy();
   });
 });
