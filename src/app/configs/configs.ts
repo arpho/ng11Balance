ff61197cf2cf41c320ef512250ead8ae4f1d507e
arpho <damicogiuseppe77@gmail.com>
2023-04-29 18:18:41 +0200

    added config properties dbName

--- src/app/configs/configs.ts
+++ src/app/configs/configs.ts
@@ -1,6 +1,7 @@
 import { RoleModel } from "../modules/user/models/privilegesLevelModel";
 
 export const configs = {
+  dbName:"myBalance",
   accessLevel: [
     new RoleModel({ key: "Sviluppatore", value: 1 }),
     new RoleModel({ key: "Responsabile", value: 2 }),
