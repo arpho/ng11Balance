bcac421448a205dbb90c10c9f76369d69dda29b2
arpho <damicogiuseppe77@gmail.com>
2023-05-12 16:56:07 +0200

    implemented storageSqliteService

---
+++ src/app/modules/offline/business/db/storageSQLiteService.ts
@@ -0,0 +1,20 @@
+import * as CordovaSQLiteDriver from 'localforage-cordovasqlitedriver'
+import { Storage } from '@ionic/storage-angular'
+import { LoadingController } from '@ionic/angular'
+import { StorageService } from './storageService'
+export class StorageSQLiteService extends StorageService {
+
+  constructor(
+    public storage: Storage,
+    public LoadingController: LoadingController
+  ) {
+    super(storage, LoadingController)
+
+  }
+  async initDB() {
+    console.log('inizializzo db sqlite')
+    await this.storage.defineDriver(CordovaSQLiteDriver)
+    this._storage = await this.storage.create()
+    console.log('sqlite db inizializzato')
+  }
+}
